21123eba017dda6e9e5a1f79bbfe30b3c9b962b8
Ricardo Prezotto <48772496+Prezt@users.noreply.github.com>
2022-12-01 10:23:09 -0300

    home page file

--- src/router.js
+++ src/router.js
@@ -1,10 +1,11 @@
 import express from "express"
+import { homepage } from "./home.js";
 import { usersController } from "./users/users-controller.js";
 
 const router = express.Router(); 
 export const appRouter = router
     .get('/', async (req, res) =>  { 
-        res.send("<html><head>API HOSTED ON /api/users/</head><body><p>Available endpoints: <ol><li>/api/users/</li><li>/api/users/:username/details</li><li>/api/users/:username/repos</li></ol></p></body></html>") 
+        res.send(homepage) 
     })
     .get('/api/users', usersController.fetchAll)
     .get('/api/users/:username/details', usersController.fetchDetailsByUsername)
