46094bb1ceea8b48c58c030f8c66170a4b834469
Ricardo Prezotto <48772496+Prezt@users.noreply.github.com>
2022-12-01 09:33:55 -0300

    better html

--- src/router.js
+++ src/router.js
@@ -4,7 +4,7 @@ import { usersController } from "./users/users-controller.js";
 const router = express.Router(); 
 export const appRouter = router
     .get('/', async (req, res) =>  { 
-        res.send("<html><head>Home</head><body><h2>API HOSTED ON /api/users/</h2><br><p>Available endpoints: <ol><li>/api/users/</li><li>/api/users/:username/details</li><li>/api/users/:username/repos</li></ol></p></body></html>") 
+        res.send("<html><head>API HOSTED ON /api/users/</head><body><p>Available endpoints: <ol><li>/api/users/</li><li>/api/users/:username/details</li><li>/api/users/:username/repos</li></ol></p></body></html>") 
     })
     .get('/api/users', usersController.fetchAll)
     .get('/api/users/:username/details', usersController.fetchDetailsByUsername)
