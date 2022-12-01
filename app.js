a450d56afb2700ea8ec5136b3ed71a023f4e415d
Ricardo Prezotto <48772496+Prezt@users.noreply.github.com>
2022-12-01 08:41:55 -0300

    adds a server file

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import { usersRouter, rootRouter } from './src/router.js'
 const app = express();
-const port = process.env.PORT || 5000;
 
 app.use('/', rootRouter)
 app.use('/api/users', usersRouter)
