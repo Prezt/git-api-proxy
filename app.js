a8be5c847be4cde3b75bd8423a025fe9dcdbdff6
Ricardo Prezotto <48772496+Prezt@users.noreply.github.com>
2022-12-01 09:32:16 -0300

    adds basic homepage

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 import express from 'express';
-import { usersRouter, rootRouter } from './src/router.js'
+import { appRouter } from './src/router.js'
 const app = express();
 
-app.use('/', rootRouter)
-app.use('/api/users', usersRouter)
+app.use('/', appRouter)
+
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    return res.send(error.message);
+});
 
 export default app
--- src/router.js
+++ src/router.js
@@ -1,11 +1,11 @@
 import express from "express"
-
-import { rootController } from "./users/root-controller.js";
 import { usersController } from "./users/users-controller.js";
 
 const router = express.Router(); 
-export const rootRouter = router.get('/', rootController.sendWelcomeMessage);
-export const usersRouter = router
-    .get('/', usersController.fetchAll)
-    .get('/:username/details', usersController.fetchDetailsByUsername)
-    .get('/:username/repos', usersController.fetchReposByUsername)
+export const appRouter = router
+    .get('/', async (req, res) =>  { 
+        res.send("<html><head>Home</head><body><h2>API HOSTED ON /api/users/</h2><br><p>Available endpoints: <ol><li>/api/users/</li><li>/api/users/:username/details</li><li>/api/users/:username/repos</li></ol></p></body></html>") 
+    })
+    .get('/api/users', usersController.fetchAll)
+    .get('/api/users/:username/details', usersController.fetchDetailsByUsername)
+    .get('/api/users/:username/repos', usersController.fetchReposByUsername)
